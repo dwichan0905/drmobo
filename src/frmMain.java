5dc31529f2bf8cead76a8b992cefec55b98e75cf
maratun1725 <marhatunsoleha17@gmail.com>
2019-07-19 21:41:49 +0700

    cmb_motheboard

--- src/frmMain.java
+++ src/frmMain.java
@@ -1,3 +1,6 @@
+
+import javax.swing.DefaultComboBoxModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,22 +29,331 @@ public class frmMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jPanel1 = new javax.swing.JPanel();
+        cmb_motherboard = new javax.swing.JComboBox<>();
+        jPanel2 = new javax.swing.JPanel();
+        cmb_Bunyi = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        jButton2 = new javax.swing.JButton();
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("dr. Mobo ver. 1.0");
+        setTitle("Dr.mobo - Sistem Pakar Analisis Masalah Motherboard Komputer");
+        setResizable(false);
+        setType(java.awt.Window.Type.UTILITY);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Merk Motherboard"));
+
+        cmb_motherboard.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "--pilih--", "AMIBIOS", "AWARD BIOS", "DELL", "IBM BIOS", "MACINTHOSH", "PHOENIX BIOS" }));
+        cmb_motherboard.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                cmb_motherboardMouseReleased(evt);
+            }
+        });
+        cmb_motherboard.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmb_motherboardActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(cmb_motherboard, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(48, 48, 48))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(cmb_motherboard, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Pola Bunyi Beep"));
+        jPanel2.setToolTipText("");
+
+        cmb_Bunyi.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(cmb_Bunyi, 0, 286, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(cmb_Bunyi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+
+        jButton1.setText("Cek");
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Hasil Analisis"));
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2)
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 265, Short.MAX_VALUE))
+        );
+
+        jButton2.setText("Tentang");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton2)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton2)
+                .addGap(19, 19, 19))
         );
+
+        jPanel2.getAccessibleContext().setAccessibleName("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void cmb_motherboardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmb_motherboardActionPerformed
+ // TODO add your handling code here:
+    }//GEN-LAST:event_cmb_motherboardActionPerformed
+
+    private void cmb_motherboardMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cmb_motherboardMouseReleased
+        // TODO add your handling code here:
+        DefaultComboBoxModel mdl = new DefaultComboBoxModel();
+        switch (cmb_motherboard.getSelectedIndex() ){
+            case 0 :
+                break;
+            case 1 :
+                mdl.addElement("1 Pendek");
+                mdl.addElement("2 Pendek");
+                mdl.addElement("3 Pendek");
+                mdl.addElement("4 Pendek");
+                mdl.addElement("5 Pendek");
+                mdl.addElement("6 Pendek");
+                mdl.addElement("7 Pendek");
+                mdl.addElement("8 Pendek");
+                mdl.addElement("9 Pendek");
+                mdl.addElement("10 Pendek");
+                mdl.addElement("11 Pendek");
+                mdl.addElement("1 Panjang, 3 Pendek ");
+                mdl.addElement("1 Panjang , 8 Pendek");
+                mdl.addElement("two-tone siren");
+                break;
+            case 2 :
+                mdl.addElement("1 Panjang, 2 Pendek");
+                mdl.addElement("1 Panjang, 3 Pendek");
+                mdl.addElement("Beep repeating endlessly");
+                mdl.addElement("Repeated high frequency beeps while PC is running");
+                mdl.addElement("Repeated beeps alternating high & low frequency");
+ 
+                break;
+            case 3 :
+                mdl.addElement("1 beeps");
+                mdl.addElement("2 beeps");
+                mdl.addElement("3 beeps");
+                mdl.addElement("4 beeps");
+                mdl.addElement("5 beeps");
+                mdl.addElement("6 beeps");
+                mdl.addElement("7 beeps");
+                
+                break;
+            case 4 :
+                mdl.addElement("No beeps");
+                mdl.addElement("1 beep pendek");
+                mdl.addElement("2 beep pendek");
+                mdl.addElement("Continuous beep");
+                mdl.addElement("Repeating short beep");
+                mdl.addElement("1 panjang dan 1 beep pendek");
+                mdl.addElement("1 panjang dan 2 beep pendek");
+                mdl.addElement("1 panjang dan 3 beep pendek");
+                mdl.addElement("3 beep panjang");
+                mdl.addElement("1 beep, blank or incorrect display");
+                break;
+            case 5 :
+                mdl.addElement("Error tone.(two sets of different tones)");
+                mdl.addElement("Star up tone, drive spins, no video");
+                mdl.addElement("Power on, no tone");
+                mdl.addElement("High tone, four higher tones");
+                break;
+            case 6 :
+                mdl.addElement("1-1-1-1");
+                mdl.addElement("1-1-1-3");
+                mdl.addElement("1-1-2-1");
+                mdl.addElement("1-1-2-3");
+                mdl.addElement("1-1-3-1");
+                mdl.addElement("1-1-3-2");
+                mdl.addElement("1-1-3-3");
+                mdl.addElement("1-1-4-1");
+                mdl.addElement("1-1-4-3");
+                mdl.addElement("1-2-1-1");
+                mdl.addElement("1-2-1-2");
+                mdl.addElement("1-2-1-3");
+                mdl.addElement("1-2-2-1");
+                mdl.addElement("1-2-2-3");
+                mdl.addElement("1-2-3-1");
+                mdl.addElement("1-2-3-3");
+                mdl.addElement("1-2-4-1");
+                mdl.addElement("1-3-1-1");
+                mdl.addElement("1-3-1-3");
+                mdl.addElement("1-3-2-1");
+                mdl.addElement("1-3-3-1");
+                mdl.addElement("1-3-3-3");
+                mdl.addElement("1-3-4-1");
+                mdl.addElement("1-3-4-3");
+                mdl.addElement("1-4-1-3");
+                mdl.addElement("1-4-2-4");
+                mdl.addElement("1-4-3-1");
+                mdl.addElement("1-4-3-2");
+                mdl.addElement("1-4-3-3");
+                mdl.addElement("1-4-4-1");
+                mdl.addElement("1-4-4-2");
+                mdl.addElement("2-1-1-1");
+                mdl.addElement("2-1-1-3");
+                mdl.addElement("2-1-2-1");
+                mdl.addElement("2-1-2-3");
+                mdl.addElement("2-1-2-4");
+                mdl.addElement("2-1-3-1");
+                mdl.addElement("2-1-3-2");
+                mdl.addElement("2-1-3-3");
+                mdl.addElement("2-1-4-1");
+                mdl.addElement("2-1-4-3");
+                mdl.addElement("2-2-1-1");
+                mdl.addElement("2-2-1-3");
+                mdl.addElement("2-2-2-1");
+                mdl.addElement("2-2-2-3");
+                mdl.addElement("2-2-3-1");
+                mdl.addElement("2-2-3-3");
+                mdl.addElement("2-2-4-1");
+                mdl.addElement("2-3-1-1");
+                mdl.addElement("2-3-1-3");
+                mdl.addElement("2-3-2-1");
+                mdl.addElement("2-3-2-3");
+                mdl.addElement("2-3-3-1");
+                mdl.addElement("2-3-3-3");
+                mdl.addElement("2-3-4-1");
+                mdl.addElement("2-3-4-3");
+                mdl.addElement("2-4-1-1");
+                mdl.addElement("2-4-1-3");
+                mdl.addElement("2-4-2-1");
+                mdl.addElement("2-4-2-3");
+                mdl.addElement("2-4-4-1");
+                mdl.addElement("2-4-4-3");
+                mdl.addElement("3-1-1-1");
+                mdl.addElement("3-1-1-3");
+                mdl.addElement("3-1-2-1");
+                mdl.addElement("3-1-2-3");
+                mdl.addElement("3-1-3-1");
+                mdl.addElement("3-1-3-3");
+                mdl.addElement("3-1-4-1");
+                mdl.addElement("3-1-4-1");
+                mdl.addElement("3-2-1-1");
+                mdl.addElement("3-2-1-2");
+                mdl.addElement("3-2-1-3");
+                mdl.addElement("3-2-2-1");
+                mdl.addElement("3-2-2-3");
+                mdl.addElement("3-2-3-1");
+                mdl.addElement("3-2-3-3");
+                mdl.addElement("3-2-4-1");
+                mdl.addElement("3-2-4-3");
+                mdl.addElement("3-3-1-1");
+                mdl.addElement("3-3-1-3");
+                mdl.addElement("3-3-3-1");
+                mdl.addElement("3-3-3-3");
+                mdl.addElement("3-3-4-1");
+                mdl.addElement("3-3-4-3");
+                mdl.addElement("3-4-1-1");
+                mdl.addElement("3-4-1-3");
+                mdl.addElement("3-4-2-1");
+                mdl.addElement("3-4-2-3");
+                mdl.addElement("3-4-3-1");
+                mdl.addElement("3-4-4-1");
+                mdl.addElement("3-4-4-3");
+                mdl.addElement("3-4-4-4");
+                
+                mdl.addElement("4-1-1-1");
+                mdl.addElement("4-2-1-1");
+                mdl.addElement("4-2-1-3");
+                mdl.addElement("4-2-2-1");
+                mdl.addElement("4-2-2-3");
+                mdl.addElement("4-2-3-1");
+                mdl.addElement("4-2-3-3");
+                mdl.addElement("4-2-4-1");
+                mdl.addElement("4-3-1-3");
+                mdl.addElement("4-3-1-4");
+                mdl.addElement("4-3-2-1");
+                
+                mdl.addElement("4-3-2-2");
+                mdl.addElement("4-3-2-3");
+                mdl.addElement("4-3-2-4");
+                mdl.addElement("4-3-3-1");
+                mdl.addElement("4-3-3-2");
+                mdl.addElement("4-3-3-3");
+                mdl.addElement("4-3-3-4");
+                mdl.addElement("4-3-4-1");
+                mdl.addElement("4-3-4-2");
+                mdl.addElement("4-3-4-3");
+                mdl.addElement("two-tone siren");
+                
+                break;    
+        
+        
+        }
+    }//GEN-LAST:event_cmb_motherboardMouseReleased
 
     /**
      * @param args the command line arguments
@@ -79,5 +391,16 @@ public class frmMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cmb_Bunyi;
+    private javax.swing.JComboBox<String> cmb_motherboard;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
     // End of variables declaration//GEN-END:variables
 }
